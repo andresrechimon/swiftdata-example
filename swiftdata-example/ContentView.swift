20e466c2e21a6f12177c41cb8216b6972bfdd5e1
Rechimon, Andrés <andres.r1995@hotmail.com>
2024-05-09 11:06:49 -0300

    Initial Commit

---
+++ swiftdata-example/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  swiftdata-example
+//
+//  Created by Andres Rechimon on 09/05/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ swiftdata-example/swiftdata_exampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftdata_exampleApp.swift
+//  swiftdata-example
+//
+//  Created by Andres Rechimon on 09/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct swiftdata_exampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
